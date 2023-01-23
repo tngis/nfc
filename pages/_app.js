3c2a3b3a0a246935df48c6522223cc60e852b0a4
Tengis Boldbaatar <tngis1413@gmail.com>
2023-01-23 17:32:19 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,34 @@
 import '@/styles/globals.css'
+import { Router } from 'next/router';
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    }
+    const end = () => {
+      setLoading(false);
+    }
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    }
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) :
+        <Component {...pageProps} />
+      }
+    </>
+  )
 }
